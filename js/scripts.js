62676a8acbf3070c85eb68f518463b0be8b412a1
ed and jl <ed@>
2017-11-28 15:26:20 -0800

    add class and created function

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  $(".term1").click(function() {
+    $(".def1").show();
+  });
+  $(".def1").click(function() {
+    $(".def1").hide();
+  });
+  $(".term2").click(function() {
+    $(".def2").show();
+  });
+  $(".def2").click(function() {
+    $(".def2").hide();
+  });
+  $(".term3").click(function() {
+    $(".def3").show();
+  });
+  $(".def3").click(function() {
+    $(".def3").hide();
+  });
+  $(".term4").click(function() {
+    $(".def4").show();
+  });
+  $(".def4").click(function() {
+    $(".def4").hide();
+  });
+  $(".term5").click(function() {
+    $(".def5").show();
+  });
+  $(".def5").click(function() {
+    $(".def5").hide();
+  });
+  $(".term6").click(function() {
+    $(".def6").show();
+  });
+  $(".def6").click(function() {
+    $(".def6").hide();
+  });
+  $(".term7").click(function() {
+    $(".def7").show();
+  });
+  $(".def7").click(function() {
+    $(".def7").hide();
+  });
+  $(".term8").click(function() {
+    $(".def8").show();
+  });
+  $(".def8").click(function() {
+    $(".def8").hide();
+  });
+});
